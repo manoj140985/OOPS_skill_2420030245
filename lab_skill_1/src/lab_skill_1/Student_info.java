61e4baaeaf9a623c840e9a8d462b4ffa097d873d
manoj140985 <kottemanoj14098510@gmail.com>
2025-08-06 09:11:50 +0530

    Add files via upload

---
+++ lab_skill_1/src/lab_skill_1/Student_info.java
@@ -0,0 +1,31 @@
+package lab_skill_1;
+
+public class Student_info {
+
+	public static void main(String[] args) 
+	{
+		        long rollNumber = 2420030245L;
+		        String name = "MANOJ";
+		        float marks = 9.3f;
+		        char gender = 'M';
+		        boolean Passed = true;
+		        
+		        	
+		        
+
+		        // Displaying the student summary
+		        System.out.println("Student Summary Slip--> ");
+		        System.out.println("Roll Number  : " + rollNumber);
+		        System.out.println("Name         : " + name);
+		        System.out.println("Gender       : " + gender);
+		        System.out.println("Marks        : " + marks);
+		        if(Passed)
+		        System.out.println("Pass Status  :PASS ");
+		        else
+		        System.out.println("Pass Status  :Fail");
+		        	
+		       
+		    }
+		
+
+}
